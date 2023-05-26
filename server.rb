d512e837e1d1a69c5b850cf8e0674cd50a8ff9a6
Dana Ma <dma@zendesk.com>
2023-05-26 14:17:12 +1000

    Handle ACKs without ~blowing up~ bed mess

--- server.rb
+++ server.rb
@@ -8,23 +8,19 @@ Socket.udp_server_loop(6969) {|msg, msg_src|
   message_unpacked = msg.unpack('nZ*Z*')
 
   pp message_unpacked
-  _op_number, filename, _mode = message_unpacked
+  op_number, filename, _mode = message_unpacked
 
-  # puts "msg_src is #{msg_src.inspect}"
-  # pp msg_src.remote_address.ip_port
-  # pp (msg_src.remote_address.methods - Object.new.methods)
-  # pp (msg_src.methods - Object.new.methods)
+  case op_number
+  when 1
+    response_data = [3, 1, File.read(filename)]
+    response_packet = response_data.pack('nnZ*')
 
-  response_data = [3, 1, File.read(filename)]
-  response_packet = response_data.pack('nnZ*')
-
-  pp response_data
-  pp response_packet
-
-  msg_src.reply response_packet
-
-  #    2. Host B sends a "DATA" (with block number= 1) to host  A  with
-  #    source= B's TID, destination= A's TID.
-  puts "Server replied?"
+    msg_src.reply response_packet
+    puts "Server replied to read request"
+  when 4
+    puts "ACK received"
+  else
+    puts "I shat the bed with op_number #{op_number}"
+  end
 }
 puts "Server dead?"
