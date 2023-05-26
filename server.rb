a284e8a3df9a8e8bd89c394998a2e6732475ce69
Dana Ma <dma@zendesk.com>
2023-05-26 14:27:19 +1000

    Slight refactor of message parsing

--- server.rb
+++ server.rb
@@ -3,24 +3,26 @@ require "pp"
 
 puts "Starting server"
 Socket.udp_server_loop(6969) {|msg, msg_src|
-  puts "Server got #{msg.inspect}"
+  puts "[INFO] Server got #{msg.inspect}"
   # n because there's some formats that are common in networks
-  message_unpacked = msg.unpack('nZ*Z*')
+  op_number, rest = msg.unpack('na*')
 
-  pp message_unpacked
-  op_number, filename, _mode = message_unpacked
+  pp op_number
 
   case op_number
   when 1
+    filename, _mode = rest.unpack('Z*Z*')
+    pp filename, _mode
+
     response_data = [3, 1, File.read(filename)]
     response_packet = response_data.pack('nnZ*')
 
     msg_src.reply response_packet
-    puts "Server replied to read request"
+    puts "[INFO] Server replied to read request"
   when 4
-    puts "ACK received"
+    puts "[INFO] ACK received"
   else
-    puts "I shat the bed with op_number #{op_number}"
+    puts "[ERROR] I shat the bed with op_number #{op_number}"
   end
 }
-puts "Server dead?"
+puts "[EMERGENCY] Server dead?"
